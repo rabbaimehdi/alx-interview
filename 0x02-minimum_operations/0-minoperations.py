a60acc68336dc9e3e0c46a060d3f90c80e4c972e
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2024-03-22 02:46:17 +0000

    Correction for task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for 0-minoperations
+"""
+
 def minOperations(n):
-    """ """
+    """ 
+    Return the fewest operations needed to result in n H characters
+    """
     number_of_op = 0
     if (n % 2 == 1):
         number_of_op = n
