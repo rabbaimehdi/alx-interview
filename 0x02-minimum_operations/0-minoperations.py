517b73177082c85168d7fb6e3ba4146b83bfef9a
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2024-03-22 02:50:45 +0000

    PEP8 style

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,24 +3,25 @@
 Module for 0-minoperations
 """
 
+
 def minOperations(n):
     """ 
     Return the fewest operations needed to result in n H characters
     """
     if n == 1:
         return 0
-    
+
     operations = 0
     clipboard = 1
     buffer = 1
-    
+
     while buffer < n:
         if n % buffer == 0:
             clipboard = buffer
             operations += 1
         buffer += clipboard
         operations += 1
-        
+
     if buffer == n:
         return operations
     else:
